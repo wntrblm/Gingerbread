91c8a98b72590866916180283b583c985c0cf2ae
Thea Flowers <thea@winterbloom.com>
2022-10-17 13:36:21 -0400

    Pull out poly output methods so that they can be used outside of outputting footprint polygons

--- zig/src/pcb.zig
+++ zig/src/pcb.zig
@@ -24,6 +24,34 @@ pub fn end_pcb(writer: anytype) !void {
     try writer.writeAll(")\n");
 }
 
+pub fn start_xx_poly(kind: []const u8, writer: anytype) !void {
+    try writer.print("  ({s}_poly\n", .{kind});
+    try writer.writeAll("    (pts\n");
+}
+
+pub fn add_xx_poly_point(pt: geometry.Point, scale_factor: f64, writer: anytype) !void {
+    try writer.print("      (xy {d:.3} {d:.3})\n", .{ pt.x * scale_factor, pt.y * scale_factor });
+}
+
+pub fn end_xx_poly(layer: []const u8, width: f64, fill: bool, writer: anytype) !void {
+    try writer.writeAll("    )\n");
+    try writer.print("    (layer \"{s}\")\n", .{layer});
+    try writer.print("    (width {d:.3})\n", .{width});
+    try writer.print("    (fill {s})\n", .{if (fill) "solid" else "none"});
+    try writer.writeAll("    (tstamp \"e9dc14e2-3c62-11ed-ab80-7a0c86e760e0\")\n");
+    try writer.writeAll("  )\n");
+}
+
+pub fn points_to_xx_poly(kind: []const u8, pts: []geometry.Point, scale_factor: f64, layer: []const u8, width: f64, fill: bool, writer: anytype) !void {
+    try start_xx_poly(kind, writer);
+
+    for (pts) |pt| {
+        try add_xx_poly_point(pt, scale_factor, writer);
+    }
+
+    try end_xx_poly(layer, width, fill, writer);
+}
+
 pub fn polylist_to_footprint(polylist: PolyList, layer: []const u8, scale_factor: f64, writer: anytype) !void {
     try writer.writeAll("(footprint \"Graphics\"\n");
     try writer.print("  (layer \"{s}\")\n", .{layer});
@@ -33,19 +61,7 @@ pub fn polylist_to_footprint(polylist: PolyList, layer: []const u8, scale_factor
     try writer.writeAll("  (tedit \"e9dc1794-3c62-11ed-ab80-7a0c86e760e0\")\n");
 
     for (polylist.items) |poly| {
-        try writer.writeAll("  (fp_poly\n");
-        try writer.writeAll("    (pts\n");
-
-        for (poly.outline) |pt| {
-            try writer.print("      (xy {d} {d})\n", .{ pt.x * scale_factor, pt.y * scale_factor });
-        }
-
-        try writer.writeAll("    )\n");
-        try writer.print("    (layer \"{s}\")\n", .{layer});
-        try writer.writeAll("    (width 0)\n");
-        try writer.writeAll("    (fill solid)\n");
-        try writer.writeAll("    (tstamp \"e9dc14e2-3c62-11ed-ab80-7a0c86e760e0\")\n");
-        try writer.writeAll("  )\n");
+        try points_to_xx_poly("fp", poly.outline, scale_factor, layer, 0, true, writer);
     }
 
     try writer.writeAll(")\n");
